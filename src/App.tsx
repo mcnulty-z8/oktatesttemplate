f0bd70a44c70e8bd0258fda251688eaeb2786b14
mcnulty-z8 <72396565+mcnulty-z8@users.noreply.github.com>
2024-10-07 19:16:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { Auth } from 'aws-amplify';
+import { auth } from "../amplify/auth/resource";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
-    Auth.currentAuthenticatedUser()
+    auth.currentAuthenticatedUser()
       .then(() => setIsAuthenticated(true))
       .catch(() => setIsAuthenticated(false));
   }, []);
@@ -27,8 +29,13 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
-  function login() {
-    Auth.federatedSignIn();
+  async function login() {
+    try {
+      await auth.signIn(username, password);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("Error signing in", error);
+    }
   }
 
   return (
@@ -51,7 +58,21 @@ function App() {
           </div>
         </>
       ) : (
-        <button onClick={login}>Login with Okta</button>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={login}>Login</button>
+        </div>
       )}
     </main>
   );
