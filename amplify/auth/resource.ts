0d04d880b15d5b63ef01fedc657f86cb05764511
mcnulty-z8 <72396565+mcnulty-z8@users.noreply.github.com>
2024-10-07 12:07:27 -0400

    Update resource.ts for Okta

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,6 @@
-import { defineAuth } from '@aws-amplify/backend';
+//amplify/auth/resource.ts
+
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +9,23 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: 'Okta',
+          clientId: '0oahln0iwgFaVZwJW1d7',
+          clientSecret: 'kikAigxnjLFHmUYCF_zFLCax2tjOpW2HT3opGvKWKDYpfLdlTTCmusDiK7CstfVi',
+          issuerUrl: 'https://simpletech.oktapreview.com', // Correct Okta domain
+        },
+      ],
+      logoutUrls: [
+        'http://localhost:5173/', 
+        'https://main.dpgverupq1lfn.amplifyapp.com/'
+      ],
+      callbackUrls: [
+        'http://localhost:5173/profile',
+        'https://main.dpgverupq1lfn.amplifyapp.com/profile',
+      ],
+    },
   },
 });
